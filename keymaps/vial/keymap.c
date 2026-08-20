2238d9cbd3b20f2a3fb30434525dce890158ff9f
kimjiho680629 <kimjiho680629@users.noreply.github.com>
2026-08-20 16:45:19 +1000

    feat: synchronize C default keymap with silakka54.vil layout

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -9,37 +9,38 @@ enum custom_keycodes {
     GAME_L_MOUSE = QK_KB_4, // Vial User 4 (마우스 좌클릭 50ms 최적 연타)
 };
 
-// 2. 키맵 (기본 레이아웃 및 커스텀 키 배치)
+// 2. 키맵 (silakka54.vil 기준 기본 레이아웃)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
-        TG(2),  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
-        KC_TAB, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
-        TG(1),  KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TG(3),
-        KC_NO,  KC_BSPC, KC_SPC, KC_ENT, KC_DEL, KC_RALT
+        QK_GESC,      KC_1,    KC_2,    KC_3,    KC_4,          KC_5,           KC_6,     KC_7,    KC_8,     KC_9,       KC_0,        TD(1),
+        LT(3, KC_TAB),KC_Q,    KC_W,    KC_E,    KC_R,          KC_T,           KC_Y,     KC_U,    KC_I,     KC_O,       KC_P,        KC_BSPC,
+        TD(0),        KC_A,    KC_S,    KC_D,    KC_F,          KC_G,           KC_H,     KC_J,    KC_K,     KC_L,       KC_SCLN,     KC_QUOT,
+        KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,           KC_N,     KC_M,    KC_COMM,  KC_DOT,     KC_SLSH,     KC_RSFT,
+        KC_LGUI,      MO(1),   LT(2, KC_SPC),                                   KC_ENT,   KC_LCTL, KC_LALT
     ),
     [1] = LAYOUT(
-        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        KC_TRNS, GAME_12345, GAME_45, GAME_L_MOUSE, KC_NO, KC_NO, KC_PGUP, KC_PGDN, KC_HOME, KC_END, KC_DEL, GAME_345,
-        KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_LBRC, KC_RBRC,
-        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, GAME_2345, KC_NO, KC_NO, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        LSFT(KC_GRV), KC_F1,   KC_F2,   KC_F3,   KC_F4,         GAME_L_MOUSE,   KC_F6,    KC_F7,   KC_F8,    KC_F9,      KC_F10,      KC_F11,
+        KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,        KC_PGUP,  KC_PGDN, KC_HOME,  KC_END,     KC_BSLS,     KC_F12,
+        KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,        KC_LEFT,  KC_DOWN, KC_UP,    KC_RGHT,    KC_LBRC,     KC_RBRC,
+        KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,        KC_TRNS,  KC_TRNS, KC_VOLD,  KC_VOLU,    KC_TRNS,     KC_DEL,
+        KC_TRNS,      KC_TRNS, KC_TRNS,                                         KC_TRNS,  KC_RCTL, KC_RALT
     ),
     [2] = LAYOUT(
-        KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE, KC_FIND, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_TRNS, KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, MS_WHLU, MS_UP, MS_WHLD, KC_NO, KC_NO,
-        KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   MS_WHLL, MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLR, KC_NO,
-        KC_TRNS, KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   MS_BTN3, MS_BTN4, MS_BTN5, KC_NO, KC_NO, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, MS_BTN1, MS_BTN2, KC_NO
+        KC_TRNS,      GAME_45, GAME_L_MOUSE, KC_TRNS, KC_TRNS,  KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,     KC_TRNS,
+        KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,        KC_PGDN,  KC_PGUP, KC_HOME,  KC_END,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,      KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,        KC_WBAK,  KC_WHOM, KC_WREF,  KC_WFWD,    KC_TRNS,     KC_TRNS,
+        KC_LEFT,      KC_DOWN, KC_RGHT, C(S(KC_C)), C(S(KC_V)), KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,     KC_TRNS,
+        KC_TRNS,      KC_TRNS, KC_TRNS,                                         KC_TRNS,  KC_TRNS, KC_TRNS
     ),
     [3] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RALT(KC_1), RSFT(KC_2), KC_MINS, KC_NO, KC_NO, KC_NO,
-        KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LSFT(KC_8), LSFT(KC_9), RALT(KC_QUOT), RALT(KC_NUHS), RALT(KC_SCLN), KC_NO,
-        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NUBS, LSFT(KC_NUBS), LSFT(KC_COMM), LSFT(KC_DOT), KC_NO, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,     KC_PWR,
+        KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,     KC_TRNS,
+        KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,     KC_TRNS,
+        KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,     KC_TRNS,
+        KC_TRNS,      KC_TRNS, KC_TRNS,                                         KC_TRNS,  KC_TRNS, KC_TRNS
     )
 };
+
 
 // 3. RGB LED 레이어 설정
 #ifdef RGBLIGHT_ENABLE
